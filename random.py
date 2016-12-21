4a80d4a3460015b6c03ff611ce1f45374fb9fe17
Ethan Harlig <ethan.harlig@gmail.com>
2016-12-20 22:36:56 -0800

    Idk

---
+++ random.py
@@ -0,0 +1,16 @@
+from slackclient import SlackClient
+import credentials
+
+BOT_NAME = 'redditbot'
+
+slack_client = SlackClient(credentials.SLACK_BOT_TOKEN)
+
+api_call = "emoji.list"
+
+if __name__ == "__main__":
+    api_call = slack_client.api_call(api_call)
+    if api_call.get('ok'):
+        # retrieve all users so we can find our bot
+        print(api_call)
+    else:
+        print("could not find bot user with the name " + BOT_NAME)
